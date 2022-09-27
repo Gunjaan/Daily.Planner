b84c349c628a94afba49b1d20c87208d05f95e96
vaibhavwin <asvaibhavsharma@gmail.com>
2022-09-27 10:28:07 +0530

    added array for todo components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 // import Backdrop from "./components/Backdrop";
+import { useState } from "react";
 import Todo from "./components/Todo";
 // import Modal from "./components/Modal";
 
 function App() {
+  const [listTodo, setListTodo] = useState([
+    "Learn React",
+    "Drink a lot of water",
+    "Blogging",
+    "Practice Leetcode",
+    "Turn off light"
+  ])
+  
   return (
     <div>
       <div className="heading">
@@ -11,14 +20,15 @@ function App() {
       <b>D a i l y P l a n n e r</b>
       <br />
       <br />
-      <Todo text="Learn React" />
-      <br />
-      <Todo text="Drink a lot of water" />
+
+      {listTodo.map(ele => <><Todo text={ele} /> <br/></>)}
+      
+      {/* <Todo text="Drink a lot of water" />
       <br />
       <Todo text="Blogging" />
       <br />
       <Todo text="Practice Leetcode" />
-      <br />
+      <br /> */}
     </div>
   );
 }
