b7e696a0f6fc79ed22900f5fb6b2f910481e6f64
Gunjan Verma <gunjan@Gunjans-MacBook-Air.local>
2022-09-27 09:51:14 +0530

    My first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+// import Backdrop from "./components/Backdrop";
+import Todo from "./components/Todo";
+// import Modal from "./components/Modal";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="heading">
+        <b> My Todo List </b>
+      </div>
+      <b>D a i l y P l a n n e r</b>
+      <br />
+      <br />
+      <Todo text="Learn React" />
+      <br />
+      <Todo text="Drink a lot of water" />
+      <br />
+      <Todo text="Blogging" />
+      <br />
+      <Todo text="Practice Leetcode" />
+      <br />
     </div>
   );
 }
